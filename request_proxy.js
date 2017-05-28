b00e452cd55ffee9304ecc6e5435f73dd40eeca8
Fabio Pacheco <nanook.labs@gmail.com>
2017-05-28 17:06:54 -0300

    melhorzinho

--- request_proxy.js
+++ request_proxy.js
@@ -1,25 +1,23 @@
-var https = require('https');
-const fs = require('fs');
+var express = require('express');
 var request = require('request');
 
 const APPID = "0411538bd3bf7707"
-const options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.crt')
-};
 
-https.createServer(options, function (req, res) {
-  if (req.url.lastIndexOf("/conditions") === 0) { // starts with
+var app = express();
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get(/^\/conditions.*/, function(req, res) {
     var base_target = "https://api.wunderground.com/api/" + APPID;
     var target = base_target + req.url;
     request(target, function (error, response, body) {
       res.writeHead(200, {'Content-Type': 'application/json'});
       res.end(body);
     })    
-  } else {
-    res.writeHead(404, {'Content-Type': 'application/json'});
-    res.end("");
-  }
-}).listen(8080);
+});
 
-console.log('Server running at https://localhost:8080/');
+app.listen(8080);
